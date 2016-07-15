7b3fa7bfdb1e33273890a23a514764b5fc2339fd
Coordinate35 <Coordinate35@hust.edu.cn>
2016-07-15 23:58:14 +0800

    77 with bug

---
+++ 77/77.c
@@ -0,0 +1,55 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *columnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+
+int** combine(int n, int k, int** columnSizes, int* returnSize) {
+    int **result;
+    int col = 0;
+    int i;
+    *returnSize = 0;
+    result = (int**)malloc((*returnSize + 1) * sizeof(int*));
+    result[0] = (int*)malloc(k * sizeof(int));
+    result[0][0] = 1;
+    while (col >= 0) {
+        while (result[*returnSize][col] > col + k + 1) {
+            col -= 1;
+            result[*returnSize][col] += 1;
+        }
+        if (col < 0) {
+            return result;
+        }
+        while (col + 1 < k) {
+            result[*returnSize][col + 1] = result[*returnSize][col] + 1;
+            col += 1;
+        }
+        while (result[*returnSize][col] <= n) {
+            *returnSize += 1;
+            result = realloc(result, (*returnSize + 1) * sizeof(int*));
+            result[*returnSize] = (int*)malloc(k * sizeof(int));
+            for (i = 0; i < k; i++) {
+                result[*returnSize][i] = result[*returnSize - 1][i];
+            }
+            result[*returnSize][col] += 1;
+        }
+    }
+    *columnSizes = (int*)malloc((*returnSize) * sizeof(int));
+    for (i = 0; i < *returnSize; i++) {
+        (*columnSizes)[i] = k;
+    }
+    return result;
+}
+
+int main() {
+    int n = 4;
+    int k = 2;
+    int **result;
+    int *columnSizes;
+    int returnSize;
+    result = combine(n, k, &columnSizes, &returnSize);
+    return 0;
+}
