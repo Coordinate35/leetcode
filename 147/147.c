2949e4ceea49926fd89c02109970b6e502614523
Coordinate35 <Coordinate35@hust.edu.cn>
2016-09-12 01:03:19 +0800

    147

--- 147/147.c
+++ 147/147.c
@@ -6,5 +6,41 @@
  * };
  */
 struct ListNode* insertionSortList(struct ListNode* head) {
-    
+    struct ListNode privot;
+    struct ListNode *current_node;
+    struct ListNode *sort_element;
+    struct ListNode *sort_element_last;
+    struct ListNode *next_current = head;
+    struct ListNode *sort_element_tail = head;
+
+    if (NULL == head) {
+        return NULL;
+    }
+
+    privot.next = head;
+    current_node = head->next;
+    next_current = current_node;
+
+    while (NULL != current_node) {
+        next_current = next_current->next;
+        
+        sort_element_last = &privot;
+        sort_element = privot.next;
+        while ((current_node != sort_element) && (current_node->val > sort_element->val)) {
+            sort_element_last = sort_element_last->next;
+            sort_element = sort_element->next;
+        }
+        
+        if (current_node != sort_element) {
+            sort_element_tail->next = current_node->next;
+            current_node->next = sort_element;
+            sort_element_last->next = current_node;
+        } else {
+            sort_element_tail = current_node;
+        }
+
+        current_node = next_current;
+    }
+
+    return privot.next;
 }
