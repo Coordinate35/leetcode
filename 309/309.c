726d711f7a41b9a6dd1d35fe5702690ac6c05a9a
Coordinate35 <Coordinate35@hust.edu.cn>
2016-11-30 10:44:18 +0800

    309 accept

--- 309/309.c
+++ 309/309.c
@@ -1,3 +1,6 @@
+/* 
+TIME LIMIT EXCEEDED O(n^3)
+
 int max(int a, int b) {
     if (a > b) {
         return a;
@@ -43,4 +46,41 @@ int maxProfit(int* prices, int pricesSize) {
         }
     }
     return f[pricesSize - 1];
+}
+*/
+
+int max(int a, int b, int c) {
+    if (a > b) {
+        if (a > c) {
+            return a;
+        }
+        return c;
+    } else {
+        if (b > c) {
+            return b;
+        }
+        return c;
+    }
+}
+
+int maxProfit(int* prices, int pricesSize) {
+    int *buy;
+    int *sell;
+    int *rest;
+    int i;
+    buy = (int*)malloc(pricesSize * sizeof(int));
+    sell = (int*)malloc(pricesSize * sizeof(int));
+    rest = (int*)malloc(pricesSize * sizeof(int));
+    for (i = 0; i < pricesSize; i++) {
+        buy[i] = 0;
+        sell[i] = 0;
+        rest[i] = 0;
+    }
+    buy[0] = 0 - prices[0];
+    for (i = 1; i < pricesSize; i++) {
+        rest[i] = max(rest[i - 1], sell[i - 1], buy[i - 1]);
+        buy[i] = max(buy[i - 1], rest[i - 1] - prices[i], INT_MIN);
+        sell[i] = max(rest[i - 1], buy[i - 1] + prices[i], INT_MIN);
+    }
+    return max(rest[pricesSize - 1], sell[pricesSize - 1], INT_MIN);
 }
